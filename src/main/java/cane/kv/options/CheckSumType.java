e0db5a42ca5061e325a48e528400421a9778552f
zhoukang <zhoukang199191@126.com>
2016-11-30 17:22:20 +0800

    修改注释模版`

--- src/main/java/cane/kv/options/CheckSumType.java
+++ src/main/java/cane/kv/options/CheckSumType.java
@@ -1,8 +1,9 @@
 /**
  * @(#)CheckSumType.java, 2016年11月30日. 
  * 
- * Copyright 2016 Yodao, Inc. All rights reserved.
- * YODAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * Copyright 2016 zhoukang, Inc. All rights reserved.
+ * Use of this source code is governed by the BSD 3-Clause License,
+ * that can be found in the LICENSE file.
  */
 package cane.kv.options;
 
