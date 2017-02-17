895cdd94330af9594dfff91f38df32a73538dcca
zhoukang <zhoukang199191@126.com>
2017-02-17 11:39:02 +0800

    增加hashtable manager

---
+++ src/main/java/cane/kv/storage/HashTableManager.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)HashTableManager.java, 2017年1月15日. 
+ * 
+ * Copyright 2017 zhoukang, Inc. All rights reserved.
+ * Use of this source code is governed by the BSD 3-Clause License,
+ * that can be found in the LICENSE file.
+ */
+package cane.kv.storage;
+
+/**
+ *
+ * @author cane
+ *
+ */
+public class HashTableManager {
+
+}
